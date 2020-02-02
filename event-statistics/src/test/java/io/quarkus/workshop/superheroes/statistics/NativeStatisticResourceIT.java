76a19ce4a09afa63cf6d9b895841bf104732f834
phgeraldeli <phgeraldeli@id.uff.br>
2020-02-02 12:04:22 -0300

    criando microservico de estatisticas

---
+++ event-statistics/src/test/java/io/quarkus/workshop/superheroes/statistics/NativeStatisticResourceIT.java
@@ -0,0 +1,9 @@
+package io.quarkus.workshop.superheroes.statistics;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeStatisticResourceIT extends StatisticResourceTest {
+
+    // Execute the same tests but in native mode.
+}
---
+++ event-statistics/src/test/java/io/quarkus/workshop/superheroes/statistics/StatisticResourceTest.java
@@ -0,0 +1,21 @@
+package io.quarkus.workshop.superheroes.statistics;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class StatisticResourceTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+          .when().get("/api/stats")
+          .then()
+             .statusCode(200)
+             .body(is("hello"));
+    }
+
+}
